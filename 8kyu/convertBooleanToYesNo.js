0d67470fc3affdd01d1e8a18e9420bded44e911a
JoseMTrias <jmgarciatrias@gmail.com>
2023-05-02 13:18:38 +0200

    convertBooleanToYesNo solution

---
+++ 8kyu/convertBooleanToYesNo.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/53369039d7ab3ac506000467
+
+// DESCRIPTION:
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// SOLUTION:
+
+function boolToWord(bool) {
+  if (bool) {
+    return 'Yes';
+  } else {
+    return 'No';
+  }
+}

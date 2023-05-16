1069cb60d9866399c765eb02f887e76d2efac176
JoseMTrias <jmgarciatrias@gmail.com>
2023-05-16 16:31:54 +0200

    squareSum solution

---
+++ 8kyu/squarenSum.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/515e271a311df0350d00000f
+
+// DESCRIPTION:
+
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9
+
+// SOLUTION:
+
+function squareSum(numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i] ** 2;
+  }
+  return sum;
+}

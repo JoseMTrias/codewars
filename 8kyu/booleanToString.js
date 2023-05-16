29ea7d29332f9e21de43fd2b80ee22cf72b7bd16
JoseMTrias <jmgarciatrias@gmail.com>
2023-05-16 14:20:31 +0200

    booleanToString solution

---
+++ 8kyu/booleanToString.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/551b4501ac0447318f0009cd
+
+// DESCRIPTION:
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+// SOLUTION:
+
+function booleanToString(b) {
+  if (b === true) {
+    return 'true';
+  } else {
+    return 'false';
+  }
+}

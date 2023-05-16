3ca4a0b3539cf1bfc5b62fec0bd991bdf7986704
JoseMTrias <jmgarciatrias@gmail.com>
2023-05-16 12:14:22 +0200

    MakeUpperCase solution

---
+++ 8kyu/MakeUpperCase.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
+
+// DESCRIPTION:
+// Write a function which converts the input string to uppercase.
+
+// SOLUTION:
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}

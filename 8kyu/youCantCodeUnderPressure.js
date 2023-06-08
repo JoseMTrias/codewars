18dcd2b66a3efbc3fa14fd72d307c29922ebf6bd
JoseMTrias <jmgarciatrias@gmail.com>
2023-06-08 10:52:57 +0200

    youCantCodeUnderPressure solution

---
+++ 8kyu/youCantCodeUnderPressure.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/53ee5429ba190077850011d4
+
+// DESCRIPTION:
+// Code as fast as you can! You need to double the integer and return it.
+
+// SOLUTION:
+
+function doubleInteger(i) {
+  return i * 2;
+}

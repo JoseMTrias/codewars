7853fd6ab4194016a77ab030fb23daef3d889f1f
JoseMTrias <jmgarciatrias@gmail.com>
2023-07-24 11:22:04 +0200

    youOnlyNeedOneBeginner solution

---
+++ 8kyu/youOnlyNeedOneBeginner.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/57cc975ed542d3148f00015b
+
+// DESCRIPTION:
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+// SOLUTION:
+
+function check(a, x) {
+  if (a.includes(x)) {
+    return true;
+  } else {
+    return false;
+  }
+}

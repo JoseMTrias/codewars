630be4515b2882028cdbedc73f4fc68090d19555
JoseMTrias <jmgarciatrias@gmail.com>
2023-07-31 16:01:25 +0200

    removing not working solution back to zero

--- 7kyu/moveZeros.js
+++ 7kyu/moveZeros.js
@@ -16,18 +16,3 @@
 // Tips: Order of other numbers should not change.
 
 // SOLUTION:
-
-// function moveZeroes(nums) {
-//     let count = 0;
-    
-//     for (let i = 0; i < nums.length; i++) {
-//       if (nums[i] !== 0) {
-//         nums[count++] = nums[i];
-//       }
-    
-//     for (let i = count; i < nums.length; i++) {
-//       nums[i] = 0;
-//     }
-//     }
-//     return nums;
-// }

a20eaa8287f4c30224a486f68ddafdba3104363b
JoseMTrias <jmgarciatrias@gmail.com>
2023-08-08 12:39:26 +0200

    replaceWithAlphabetPosition solution

--- 6kyu/replaceWithAlphabetPosition.js
+++ 6kyu/replaceWithAlphabetPosition.js
@@ -14,3 +14,14 @@
 // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
 
 // SOLUTION:
+
+function alphabetPosition(text) {
+  let onlyLetters = text
+    .toLowerCase()
+    .split('')
+    .filter((char) => /[a-zA-Z]/.test(char));
+  for (let i = 0; i < onlyLetters.length; i++) {
+    onlyLetters[i] = onlyLetters[i].charCodeAt(0) - 96;
+  }
+  return String(onlyLetters.join(' '));
+}

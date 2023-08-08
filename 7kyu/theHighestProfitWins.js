0521acdf4785113a73abe49b241f5bc76f8170ca
JoseMTrias <jmgarciatrias@gmail.com>
2023-08-08 16:13:13 +0200

    theHiguestProfitWins solutions

--- 7kyu/theHighestProfitWins.js
+++ 7kyu/theHighestProfitWins.js
@@ -2,8 +2,8 @@
 
 // DESCRIPTION:
 // Story
-// Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, this wouldn't give 
-// him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest 
+// Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, this wouldn't give
+// him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest
 // possible price and sell it at the highest.
 
 // Task
@@ -14,8 +14,21 @@
 // [2334454,5] --> [5,2334454]
 // [1]         --> [1,1]
 // Remarks
-// All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will 
+// All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will
 // always get an array or a list, you don't have to check for null, undefined or similar.
 
 // SOLUTION:
 
+function minMax(arr) {
+  let result = [];
+  let sorted = arr.sort((a, b) => a - b);
+  result.push(sorted[0]);
+  result.push(sorted[sorted.length - 1]);
+  return result;
+}
+
+// BETTER SOLUTION:
+
+function minMax(arr) {
+  return [Math.min(...arr), Math.max(...arr)];
+}

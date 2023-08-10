7200eb54a342b5992cf1112bf448d2e3979cf9e1
JoseMTrias <122265803+JoseMTrias@users.noreply.github.com>
2023-08-10 16:27:57 +0200

    makeAFunctionThatDoesArithmetic solution

--- 7kyu/makeAFunctionThatDoesArithmetic.js
+++ 7kyu/makeAFunctionThatDoesArithmetic.js
@@ -18,3 +18,13 @@
 // Try to do it without using if statements!
 
 // SOLUTION:
+
+function arithmetic(a, b, operator){
+  let operations = {
+    add: a + b,
+    subtract: a - b,
+    multiply: a * b,
+    divide: a / b
+  }
+  return operations[operator]
+}

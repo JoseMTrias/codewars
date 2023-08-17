a417f1d8ea97e4be65377205c5ef775fb7390397
JoseMTrias <jmgarciatrias@gmail.com>
2023-08-17 15:28:26 +0200

    catYearsDogYears solution

--- 8kyu/catYearsDogYears.js
+++ 8kyu/catYearsDogYears.js
@@ -24,3 +24,18 @@
 
 // SOLUTION:
 
+var humanYearsCatYearsDogYears = function (humanYears) {
+  let catYears;
+  let dogYears;
+  if (humanYears === 1) {
+    catYears = 15;
+    dogYears = 15;
+  } else if (humanYears === 2) {
+    catYears = 24;
+    dogYears = 24;
+  } else if (humanYears > 2) {
+    catYears = (humanYears - 2) * 4 + 24;
+    dogYears = (humanYears - 2) * 5 + 24;
+  }
+  return [humanYears, catYears, dogYears];
+};

79615a89f326b19a7efc07e06dcd780e809cf530
JoseMTrias <jmgarciatrias@gmail.com>
2023-09-18 18:23:14 +0200

    simpleMultiplication start foreal

---
+++ 8kyu/simpleMultipication.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/583710ccaa6717322c000105
+
+// DESCRIPTION:
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+// SOLUTION:
+

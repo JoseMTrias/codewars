39ad886f49e60994a6e662bb187f9393c2b8eb82
JoseMTrias <jmgarciatrias@gmail.com>
2023-09-27 19:10:15 +0200

    findTheDivisors solution

--- 7kyu/findTheDivisors.js
+++ 7kyu/findTheDivisors.js
@@ -9,3 +9,11 @@
 // divisors(13); // should return "13 is prime"
 
 // SOLUTION:
+
+function divisors(integer) {
+  let res = [];
+  for (let i = 2; i < integer; i++) {
+    if (integer % i === 0) res.push(i);
+  }
+  return res.length !== 0 ? res : `${integer} is prime`;
+}

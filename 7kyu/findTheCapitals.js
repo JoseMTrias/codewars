11fc77a80e593f8ed211a899db62faafc02fcaf1
JoseMTrias <jmgarciatrias@gmail.com>
2023-10-23 19:06:48 +0200

    findTheCapitals start

---
+++ 7kyu/findTheCapitals.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/539ee3b6757843632d00026b
+
+// DESCRIPTION:
+// Instructions
+// Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+// Example (Input --> Output)
+// "CodEWaRs" --> [0,3,4,6]
+
+// SOLUTION:

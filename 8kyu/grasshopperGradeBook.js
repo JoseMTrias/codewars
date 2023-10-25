8b0f23a39fef649fe5495d028a48d840f5706a4c
JoseMTrias <jmgarciatrias@gmail.com>
2023-10-25 17:46:24 +0200

    grasshopperGradeBook solution

--- 8kyu/grasshopperGradeBook.js
+++ 8kyu/grasshopperGradeBook.js
@@ -14,3 +14,12 @@
 
 // SOLUTION:
 
+function getGrade(s1, s2, s3) {
+  let score = (s1 + s2 + s3) / 3;
+  console.log(score);
+  if (score < 60) return 'F';
+  if (score < 70) return 'D';
+  if (score < 80) return 'C';
+  if (score < 90) return 'B';
+  if (score <= 100) return 'A';
+}

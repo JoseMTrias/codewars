95d9c48cd896d9d84f770dab73c6c6e2fa519d43
JoseMTrias <jmgarciatrias@gmail.com>
2023-10-30 18:17:52 +0100

    function2 start

---
+++ 8kyu/function2SquaringAnArgument.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/523b623152af8a30c6000027
+
+// DESCRIPTION:
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// SOLUTION:

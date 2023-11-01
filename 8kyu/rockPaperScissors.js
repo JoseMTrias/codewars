9ffc073971dc7a9df0237dbb572b2297d5fa4349
JoseMTrias <jmgarciatrias@gmail.com>
2023-11-01 22:39:42 +0100

    rockPaperScissors start

---
+++ 8kyu/rockPaperScissors.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/5672a98bdbdd995fad00000f
+
+// DESCRIPTION:
+// Rock Paper Scissors
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples(Input1, Input2 --> Output):
+
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+// SOLUTION:
+

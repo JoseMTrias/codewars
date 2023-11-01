8e04cad9c83cfcfe9fef24edbd0fc825bd56d34b
JoseMTrias <jmgarciatrias@gmail.com>
2023-11-01 22:15:42 +0100

    removeDuplicates starttt

--- 8kyu/removeDuplicatesFromList.js
+++ 8kyu/removeDuplicatesFromList.js
@@ -1,13 +1,14 @@
-https://www.codewars.com/kata/57a5b0dfcf1fa526bb000118
+// https://www.codewars.com/kata/57a5b0dfcf1fa526bb000118
 
-DESCRIPTION:
-Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+// DESCRIPTION:
+// Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
 
-The order of the sequence has to stay the same.
+// The order of the sequence has to stay the same.
 
-Examples:
+// Examples:
 
-Input -> Output
-[1, 1, 2] -> [1, 2]
-[1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+// Input -> Output
+// [1, 1, 2] -> [1, 2]
+// [1, 2, 1, 1, 3, 2] -> [1, 2, 3]
 
+// SOLUTION:

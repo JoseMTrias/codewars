18eb1be8989b4fea57778e00b23b6ef62ca76539
JoseMTrias <jmgarciatrias@gmail.com>
2023-11-05 18:24:14 +0100

    areTheyTheSame solution

--- 6kyu/areTheyTheSame.js
+++ 6kyu/areTheyTheSame.js
@@ -5,20 +5,20 @@
 
 // Examples
 // Valid arrays
-// a = [121, 144, 19, 161, 19, 144, 19, 11]  
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
 // b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
 // comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
 
-// a = [121, 144, 19, 161, 19, 144, 19, 11] 
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
 // b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
 // Invalid arrays
 // If, for example, we change the first number to something else, comp is not returning true anymore:
 
-// a = [121, 144, 19, 161, 19, 144, 19, 11]  
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
 // b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
 // comp(a,b) returns false because in b 132 is not the square of any number of a.
 
-// a = [121, 144, 19, 161, 19, 144, 19, 11]  
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
 // b = [121, 14641, 20736, 36100, 25921, 361, 20736, 361]
 // comp(a,b) returns false because in b 36100 is not the square of any number of a.
 
@@ -28,3 +28,10 @@
 // If a or b are nil (or null or None, depending on the language), the problem doesn't make sense so return false.
 
 // SOLUTION:
+
+function comp(array1, array2) {
+  if (!array1 || !array2) return false;
+  array1 = array1.map((e) => e * e).sort((a, b) => b - a);
+  array2 = array2.sort((a, b) => b - a);
+  return array1.toString() === array2.toString();
+}

b2ed55e9f2033460f2a7e7b99765c174492757f6
JoseMTrias <jmgarciatrias@gmail.com>
2023-11-13 16:55:13 +0100

    cantSleepCountSheep solution

--- 8kyu/cantSleepCountSheep.js
+++ 8kyu/cantSleepCountSheep.js
@@ -6,7 +6,12 @@
 // Task:
 // Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
 
-
-
 // SOLUTION:
 
+var countSheep = function (num) {
+  let count = '';
+  for (let i = 1; i <= num; i++) {
+    count += i + ' sheep...';
+  }
+  return count;
+};

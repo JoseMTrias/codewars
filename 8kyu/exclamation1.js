d42f4bb1d747a1d4b32b3def5cf361551c06969b
JoseMTrias <jmgarciatrias@gmail.com>
2023-11-14 21:52:48 +0100

    exclamation1 start

---
+++ 8kyu/exclamation1.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/57fae964d80daa229d000126
+
+// DESCRIPTION:
+// Description:
+// Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+// Examples
+// "Hi!"     ---> "Hi"
+// "Hi!!!"   ---> "Hi!!"
+// "!Hi"     ---> "!Hi"
+// "!Hi!"    ---> "!Hi"
+// "Hi! Hi!" ---> "Hi! Hi"
+// "Hi"      ---> "Hi"
+
+// SOLUTION:
+

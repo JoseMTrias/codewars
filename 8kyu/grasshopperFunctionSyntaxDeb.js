7c9529ac89a52342da32773ee7719bc9658f2611
JoseMTrias <jmgarciatrias@gmail.com>
2023-11-14 21:25:50 +0100

    grasshoperFunctionSyntaxDeb solution

--- 8kyu/grasshopperFunctionSyntaxDeb.js
+++ 8kyu/grasshopperFunctionSyntaxDeb.js
@@ -5,3 +5,7 @@
 // A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
 
 // SOLUTION:
+
+function main(verb, noun) {
+  return verb + noun;
+}

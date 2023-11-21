8ec780b645f01c1c405ef14f2fa28873b35ac2af
JoseMTrias <jmgarciatrias@gmail.com>
2023-11-21 16:45:55 +0100

    enumMagicDoesListIncludeThis start

---
+++ 8kyu/enumMagicDoesListIncludeThis.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/545991b4cbae2a5fda000158
+
+// DESCRIPTION:
+// Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+// SOLUTION:
+

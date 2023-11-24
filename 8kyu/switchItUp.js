d815f9dcd42d906d733361f3a54b79b072999b6b
JoseMTrias <jmgarciatrias@gmail.com>
2023-11-24 17:41:00 +0100

    switchItUp start

---
+++ 8kyu/switchItUp.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/5808dcb8f0ed42ae34000031
+
+// DESCRIPTION:
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+// If your language supports it, try using a switch statement.
+
+// SOLUTION:

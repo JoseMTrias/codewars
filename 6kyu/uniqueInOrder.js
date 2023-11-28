b15cefd7b2e237adc7f24de0b66b8839a0ea1bd6
JoseMTrias <jmgarciatrias@gmail.com>
2023-11-28 17:36:35 +0100

    uniqueInOrder solution

--- 6kyu/uniqueInOrder.js
+++ 6kyu/uniqueInOrder.js
@@ -11,3 +11,12 @@
 
 // SOLUTION:
 
+function uniqueInOrder(iterable) {
+  let res = [];
+  for (let i = 0; i < iterable.length; i++) {
+    if (iterable[i] !== iterable[i + 1]) {
+      res.push(iterable[i]);
+    }
+  }
+  return res;
+}

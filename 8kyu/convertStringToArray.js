34a38771c4ff1cfbdf1e529aed37a04889c44035
JoseMTrias <jmgarciatrias@gmail.com>
2023-12-01 12:37:41 +0100

    converStringToArray solution

--- 8kyu/convertStringToArray.js
+++ 8kyu/convertStringToArray.js
@@ -9,3 +9,7 @@
 // "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
 
 // SOLUTION:
+
+function stringToArray(string) {
+  return string.split(' ');
+}

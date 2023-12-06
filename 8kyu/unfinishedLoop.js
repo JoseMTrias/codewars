6b3c902394af88348c0864fa20a036a72f6fe14b
JoseMTrias <jmgarciatrias@gmail.com>
2023-12-06 18:51:57 +0100

    unfinishedLoop start

---
+++ 8kyu/unfinishedLoop.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/55c28f7304e3eaebef0000da
+
+
+// Collect|
+// DESCRIPTION:
+// Unfinished Loop - Bug Fixing #1
+// Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+
+// SOLUTION:

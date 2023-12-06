914279dce36479628d1bc766952d5d8fcc54c385
JoseMTrias <jmgarciatrias@gmail.com>
2023-12-06 18:55:25 +0100

    unfinishedLoop solution

--- 8kyu/unfinishedLoop.js
+++ 8kyu/unfinishedLoop.js
@@ -1,9 +1,16 @@
 // https://www.codewars.com/kata/55c28f7304e3eaebef0000da
 
-
 // Collect|
 // DESCRIPTION:
 // Unfinished Loop - Bug Fixing #1
 // Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
 
 // SOLUTION:
+
+function createArray(number) {
+  let newArray = [];
+  for (let counter = 1; counter <= number; counter++) {
+    newArray.push(counter);
+  }
+  return newArray;
+}

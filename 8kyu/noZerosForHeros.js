ffcfedffb0216a80b0f7e8f911f8e1297eeb6816
JoseMTrias <jmgarciatrias@gmail.com>
2024-01-09 22:01:26 +0100

    noZerosForHeros solution

--- 8kyu/noZerosForHeros.js
+++ 8kyu/noZerosForHeros.js
@@ -13,3 +13,15 @@
 // -1050 -> -105
 
 // SOLUTION:
+
+function noBoringZeros(n) {
+  if (n === 0) return 0;
+  let arr = n.toString().split('');
+  for (let i = arr.length; i > 0; i--) {
+    if (arr[arr.length - 1] == '0') {
+      arr.pop();
+    } else {
+      return +arr.join('');
+    }
+  }
+}

67604ac6bea7c7391b514d8de2f9fd04f950479e
JoseMTrias <jmgarciatrias@gmail.com>
2024-01-11 18:53:48 +0100

    alphabetWar solution

--- 7kyu/alphabetWar.js
+++ 7kyu/alphabetWar.js
@@ -29,3 +29,21 @@
 // alphabetWar("wwwwwwz");  //=> Left side wins!
 
 // SOLUTION:
+
+function alphabetWar(fight) {
+  let left = 0;
+  let right = 0;
+  for (let i = 0; i < fight.length; i++) {
+    if (fight[i] === 'w') left += 4;
+    if (fight[i] === 'p') left += 3;
+    if (fight[i] === 'b') left += 2;
+    if (fight[i] === 's') left += 1;
+    if (fight[i] === 'm') right += 4;
+    if (fight[i] === 'q') right += 3;
+    if (fight[i] === 'd') right += 2;
+    if (fight[i] === 'z') right += 1;
+  }
+  if (left === right) return "Let's fight again!";
+  if (left > right) return 'Left side wins!';
+  if (left < right) return 'Right side wins!';
+}

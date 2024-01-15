37fef6b0e76ac84daa501117ad9c3999f125f1d5
JoseMTrias <jmgarciatrias@gmail.com>
2024-01-15 18:05:25 +0100

    maximumProduct start

---
+++ 7kyu/maximumProduct.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/5a4138acf28b82aa43000117
+
+// DESCRIPTION:
+// Task
+// Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers in the array.
+
+// Notes
+// Array/list size is at least 2.
+
+// Array/list numbers could be a mixture of positives, negatives also zeroes .
+
+// Input >> Output Examples
+// adjacentElementsProduct([1, 2, 3]); ==> return 6
+
+// SOLUTION:

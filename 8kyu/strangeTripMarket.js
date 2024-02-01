e5c00847a869fc5012a04228c01e34be4e74f48e
JoseMTrias <jmgarciatrias@gmail.com>
2024-02-01 16:22:34 +0100

    strangeTripMarket solution

--- 8kyu/strangeTripMarket.js
+++ 8kyu/strangeTripMarket.js
@@ -9,5 +9,8 @@
 
 // Since Nessie is a master of disguise, the only way accurately tell is to look for the phrase "tree fiddy". Since you are tired of being grifted by this monster, the time has come to code a solution for finding The Loch Ness Monster. Note that the phrase can also be written as "3.50" or "three fifty". Your function should return true if you're talking with The Loch Ness Moster, false otherwise.
 
-
 // SOLUTION:
+
+function isLockNessMonster(s) {
+  return /tree fiddy|3.50|three fifty/.test(s);
+}

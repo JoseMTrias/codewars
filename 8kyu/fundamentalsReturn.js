a9ae82fe58d26bf56dabf0ffc6aa57227a773e5c
JoseMTrias <jmgarciatrias@gmail.com>
2024-02-02 11:19:06 +0100

    fundamentalsReturn solution

--- 8kyu/fundamentalsReturn.js
+++ 8kyu/fundamentalsReturn.js
@@ -20,3 +20,27 @@
 // Note: All math operations will be: a (operation) b
 
 // SOLUTION:
+
+function add(a, b) {
+  return a + b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function mod(a, b) {
+  return a % b;
+}
+
+function exponent(a, b) {
+  return a ** b;
+}
+
+function subt(a, b) {
+  return a - b;
+}

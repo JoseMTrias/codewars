f1c01940dfe0cee22cb746c7dd79676c1e8b078e
JoseMTrias <jmgarciatrias@gmail.com>
2024-02-06 11:23:45 +0100

    theVowelCode solution

--- 6kyu/theVowelCode.js
+++ 6kyu/theVowelCode.js
@@ -17,3 +17,40 @@
 // For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.
 
 // SOLUTION:
+
+function encode(str) {
+  let splitted = str.split('');
+  for (let i = 0; i < splitted.length; i++) {
+    if (splitted[i] === 'a') {
+      splitted[i] = splitted[i].replace('a', 1);
+    } else if (splitted[i] === 'e') {
+      splitted[i] = splitted[i].replace('e', 2);
+    } else if (splitted[i] === 'i') {
+      splitted[i] = splitted[i].replace('i', 3);
+    } else if (splitted[i] === 'o') {
+      splitted[i] = splitted[i].replace('o', 4);
+    } else if (splitted[i] === 'u') {
+      splitted[i] = splitted[i].replace('u', 5);
+    }
+  }
+  return splitted.join('');
+}
+
+function decode(str) {
+  let splitted = str.split('');
+  console.log(splitted);
+  for (let i = 0; i < splitted.length; i++) {
+    if (splitted[i] === '1') {
+      splitted[i] = splitted[i].replace(1, 'a');
+    } else if (splitted[i] === '2') {
+      splitted[i] = splitted[i].replace(2, 'e');
+    } else if (splitted[i] === '3') {
+      splitted[i] = splitted[i].replace(3, 'i');
+    } else if (splitted[i] === '4') {
+      splitted[i] = splitted[i].replace(4, 'o');
+    } else if (splitted[i] === '5') {
+      splitted[i] = splitted[i].replace(5, 'u');
+    }
+  }
+  return splitted.join('');
+}

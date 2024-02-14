fd427e0051e99afd8cd5c655ab68104da18723f5
JoseMTrias <jmgarciatrias@gmail.com>
2024-02-14 10:13:10 +0100

    factorial2 start

---
+++ 7kyu/factorial2.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/57a049e253ba33ac5e000212
+
+// DESCRIPTION:
+// Your task is to write function factorial.
+
+// https://en.wikipedia.org/wiki/Factorial
+
+// SOLUTION:

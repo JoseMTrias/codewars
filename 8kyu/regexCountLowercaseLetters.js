b3cf58f4afd047c171f6ad2f56d21a4602d93dd1
JoseMTrias <jmgarciatrias@gmail.com>
2024-02-16 07:57:36 +0100

    regexCountLowercaseLetters start

---
+++ 8kyu/regexCountLowercaseLetters.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/56a946cd7bd95ccab2000055
+
+// DESCRIPTION:
+// Your task is simply to count the total number of lowercase letters in a string.
+
+// Examples
+// "abc" ===> 3
+
+// "abcABC123" ===> 3
+
+// "abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~" ===> 3
+
+// "" ===> 0;
+
+// "ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~" ===> 0
+
+// "abcdefghijklmnopqrstuvwxyz" ===> 26
+
+// SOLUTION:

f9818ce9f88fa3961dd02898ebfd683728f9efb3
JoseMTrias <jmgarciatrias@gmail.com>
2024-04-02 15:51:46 +0200

    isItAPalindrome start

---
+++ 8kyu/isItAPalindrome.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/57a1fd2ce298a731b20006a4
+
+// DESCRIPTION:
+// Write a function that checks if a given string (case insensitive) is a palindrome.
+
+// A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as madam or racecar.
+
+// SOLUTION:

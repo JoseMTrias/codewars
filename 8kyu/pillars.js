1a4863843a273c371001080673c9cb98b69f17ad
JoseMTrias <jmgarciatrias@gmail.com>
2024-05-12 22:19:32 +0200

    pillars solution

--- 8kyu/pillars.js
+++ 8kyu/pillars.js
@@ -9,3 +9,8 @@
 // Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
 
 // SOLUTION:
+
+function pillars(numPill, dist, width) {
+  if (numPill === 1) return 0;
+  return numPill * width - 2 * width + dist * 100 * (numPill - 1);
+}

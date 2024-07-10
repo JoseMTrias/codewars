f5d7034715385ced6c728728928c13167bf78844
JoseMTrias <jmgarciatrias@gmail.com>
2024-07-10 09:58:13 +0200

    getAscciValue start

---
+++ 8kyu/getAsciiValue.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/55acfc59c3c23d230f00006d
+
+// DESCRIPTION:
+// Get ASCII value of a character.
+
+// For the ASCII table you can refer to http://www.asciitable.com/
+
+// SOLUTION:

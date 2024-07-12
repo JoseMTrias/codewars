a7123bdb3dcf50861cd5a2e8c9fa91e198f5167d
JoseMTrias <jmgarciatrias@gmail.com>
2024-07-12 15:55:01 +0200

    hexToDecimal start

---
+++ 8kyu/hexToDecimal.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/57a4d500e298a7952100035d
+
+// DESCRIPTION:
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+// SOLUTION:

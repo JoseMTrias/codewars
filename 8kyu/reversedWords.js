5a5addb5d2bb714ff9694169b87869a3cf539646
JoseMTrias <jmgarciatrias@gmail.com>
2024-11-06 19:56:32 +0100

    reversedWords solution

--- 8kyu/reversedWords.js
+++ 8kyu/reversedWords.js
@@ -10,3 +10,7 @@
 // "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
 
 // SOLUTION:
+
+function reverseWords(str) {
+  return str.split(' ').reverse().join(' ');
+}

b2bb2f885cd44992fadc8505b45b8233f9eec73b
JoseMTrias <jmgarciatrias@gmail.com>
2025-03-12 00:32:12 +0100

    fixBeforeGardenDies solution

--- 8kyu/fixBeforeGardenDies.js
+++ 8kyu/fixBeforeGardenDies.js
@@ -6,3 +6,11 @@
 // Your task is to debug the code before your plants die!
 
 // SOLUTION:
+
+function rainAmount(mm) {
+  if (mm < 40) {
+    return 'You need to give your plant ' + (40 - mm) + 'mm of water';
+  } else {
+    return 'Your plant has had more than enough water for today!';
+  }
+}

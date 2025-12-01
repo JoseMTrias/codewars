6228369ecbfc0e36c07fac86e3f909b3c2163dfe
JoseM GT <jmgarciatrias@gmail.com>
2025-12-01 10:15:10 +0100

    swapValues solution

--- 8kyu/swapValues.js
+++ 8kyu/swapValues.js
@@ -6,5 +6,11 @@
 
 // Can you figure out what's wrong here?
 
-
 // SOLUTION:
+
+function swapValues() {
+  var args = Array.prototype.slice.call(arguments);
+  var temp = args[0][0];
+  args[0][0] = args[0][1];
+  args[0][1] = temp;
+}

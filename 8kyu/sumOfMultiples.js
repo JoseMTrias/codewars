0c273fdbc21a8853a71161fee1f140f5292d1a59
JoseM GT <jmgarciatrias@gmail.com>
2026-01-28 14:21:43 +0100

    sumOfMultiples solution

--- 8kyu/sumOfMultiples.js
+++ 8kyu/sumOfMultiples.js
@@ -15,3 +15,11 @@
 // sumMul(4, -7)  ==> "INVALID"
 
 // SOLUCION:
+
+function sumMul(n, m) {
+  let result = 0;
+  for (let i = 1; i * n < m; i++) {
+    result += i * n;
+  }
+  return result > 0 ? result : "INVALID";
+}
